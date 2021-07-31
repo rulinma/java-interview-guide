0b3a5095f3ef44df4bfc6093c72f9cc6d2e38a15
rulinma <rulinma@gmail.com>
2021-07-31 18:28:23 +0800

    add softreference

---
+++ src/main/java/com/rulinma/interview/voliatedemo/PhantomReferenceDemo.java
@@ -0,0 +1,32 @@
+package com.rulinma.interview.voliatedemo;
+
+import java.lang.ref.PhantomReference;
+import java.lang.ref.ReferenceQueue;
+
+/**
+ * @author rollin
+ */
+public class PhantomReferenceDemo {
+    public static void main(String[] args) {
+        Object o1 = new Object();
+        ReferenceQueue<Object> referenceQueue = new ReferenceQueue<>();
+        PhantomReference<Object> phantomReference = new PhantomReference<>(o1, referenceQueue);
+        System.out.println("***************GC回收前***************");
+        System.out.println(o1);
+        System.out.println(phantomReference.get());
+        System.out.println(referenceQueue.poll());
+
+        System.out.println("***************启动GC***************");
+        o1 = null;
+        System.gc();
+        try {
+            Thread.sleep(500);
+            //确保GC都执行完了
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(o1);
+        System.out.println(phantomReference.get());
+        System.out.println(referenceQueue.poll());
+    }
+}
---
+++ src/main/java/com/rulinma/interview/voliatedemo/SoftReferenceDemo.java
@@ -0,0 +1,55 @@
+package com.rulinma.interview.voliatedemo;
+
+import java.lang.ref.SoftReference;
+
+public class SoftReferenceDemo {
+
+    public static void main(String[] args) {
+//        test();
+        User obj = new User();
+        obj.setName("test");
+
+        SoftReference<User> softReference = new SoftReference<>(obj);
+
+        System.out.println(obj);
+        System.out.println(softReference.get());
+        obj = null;
+        System.gc();
+
+        System.out.println(obj);
+        System.out.println(softReference.get());
+    }
+
+    private static void test() {
+        Object obj = new Object();
+
+        SoftReference<Object> softReference = new SoftReference<>(obj);
+
+        System.out.println(obj);
+        System.out.println(softReference.get());
+        obj = null;
+        System.gc();
+
+        System.out.println(softReference.get());
+    }
+
+}
+
+class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
