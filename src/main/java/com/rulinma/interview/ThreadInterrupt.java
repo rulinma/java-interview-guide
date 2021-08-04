3fdd19c3c512878e0d3887e10d8d7ecf41574d56
rulinma <rulinma@gmail.com>
2021-08-04 11:45:35 +0800

    重构

--- src/main/java/com/rulinma/interview/ThreadInterrupt.java
+++ src/main/java/com/rulinma/interview/ThreadInterrupt.java
@@ -8,12 +8,16 @@ public class ThreadInterrupt {
     public static void main(String[] args) {
         Thread thread = new Thread(() -> {
             while (!Thread.interrupted()) {
-                System.out.println("xxx");
+                System.out.println("xxx:" + Thread.interrupted());
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
+//                    System.out.println("xxx1:" + Thread.interrupted());
+                    // 收到中断信息
                     Thread.currentThread().interrupt();
+                    // true
+//                    System.out.println("xxx2:" + Thread.interrupted());
                 }
             }
             System.out.println("yyy");
