9409e1db9b1d57abb880480738397b2e9976499e
rulinma <rulinma@gmail.com>
2021-08-04 12:22:08 +0800

    重构

--- src/main/java/com/rulinma/interview/ThreadInterrupt.java
+++ src/main/java/com/rulinma/interview/ThreadInterrupt.java
@@ -7,15 +7,19 @@ public class ThreadInterrupt {
 
     public static void main(String[] args) {
         Thread thread = new Thread(() -> {
-            while (!Thread.interrupted()) {
-                System.out.println("xxx:" + Thread.interrupted());
+            while (!Thread.currentThread().isInterrupted())
+            {
+                System.out.println("xxx:" + Thread.currentThread().isInterrupted());
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
+                    // 捕获异常后就自动重置了flag？
                     e.printStackTrace();
-//                    System.out.println("xxx1:" + Thread.interrupted());
+                    System.out.println("xxx1:" + Thread.currentThread().isInterrupted());
                     // 收到中断信息
+                    // restore interrupt
                     Thread.currentThread().interrupt();
+                    System.out.println("xxx2:" + Thread.currentThread().isInterrupted());
                     // true
 //                    System.out.println("xxx2:" + Thread.interrupted());
                 }
@@ -31,5 +35,7 @@ public class ThreadInterrupt {
             e.printStackTrace();
         }
         thread.interrupt();
+        System.out.println("vv:" + thread.isInterrupted());
+        System.out.println("end");
     }
 }
